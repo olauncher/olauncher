b33ef166e7139c30ebd8e24cded461d6ed7fed19
bigfoot547 <bigfoot@figboot.dev>
2024-03-09 20:33:24 -0600

    fix java runtime bug

--- rebuildPatches.sh
+++ rebuildPatches.sh
@@ -2,6 +2,26 @@
 
 basedir=$(pwd)
 
+cleanupPatches() {
+  pushd "$1"
+  for patch in *.patch; do
+    echo "$patch"
+    gitver=$(tail -n 2 "$patch" | grep -ve "^$" | tail -n 1)
+    diffs=$(git diff --staged "$patch" | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)")
+
+    testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
+    if [ "x$testver" != "x" ]; then
+        diffs=$(echo "$diffs" | sed 'N;$!P;$!D;$d')
+    fi
+
+    if [ "x$diffs" == "x" ] ; then
+        git reset HEAD "$patch" >/dev/null
+        git checkout -- "$patch" >/dev/null
+    fi
+  done
+  popd
+}
+
 rebuildPatches() {
     from=$1
     to=$2
@@ -10,6 +30,9 @@ rebuildPatches() {
     pushd "$to"
     git format-patch --no-stat -N -o "$basedir/${from}-patches" origin/HEAD
     popd
+
+    git add "${from}-patches"/*.patch
+    cleanupPatches "${from}-patches"
 }
 
 rebuildPatches launcher olauncher
